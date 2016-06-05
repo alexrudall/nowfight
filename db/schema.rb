f65f2dfe7e5bcf48b4a1e0ddbc4317ab04b54cdd
Alex Rudall <alexrudall@gmail.com>
2016-06-05 11:36:35 +0100

    Add Conversation

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604173746) do
+ActiveRecord::Schema.define(version: 20160604223003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.string   "opinion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
